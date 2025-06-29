c71ebf50301e41fc590dcf8c0ceabe35932b0f55
Franciszek Andruszkiewicz <franciszek.andruszkiewicz@komputronik.pl>
2025-06-29 23:29:55 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@ import '@/../node_modules/bootstrap/scss/bootstrap.scss';
 import "@/../node_modules/bootstrap-icons/font/bootstrap-icons.css";
 import '@/assets/main.scss'
 import TheMain from './views/TheMain.vue'
-
-
 </script>
 
 <template>
